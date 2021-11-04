5acb6749d45927532993e82f7920e8f942c856f5
Dragxfly <59048803+JannisJost@users.noreply.github.com>
2021-11-04 10:04:48 +0100

    Initial commit

---
+++ src/main/java/com/bbbaden/preparation_lb133/XMLManager.java
@@ -0,0 +1,27 @@
+package com.bbbaden.preparation_lb133;
+
+import java.io.IOException;
+import javax.faces.context.FacesContext;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+/**
+ *
+ * @author Jannis
+ */
+public class XMLManager {
+
+    public XMLManager() {
+        try {
+            String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/");
+            path = path + "WEB-INF\\Log.xml";
+            Document doc = new SAXBuilder().build(path);
+
+        } catch (IOException | JDOMException e) {
+            System.err.println("ahhhhhhhhhh" +e);
+        }
+
+    }
+
+}
